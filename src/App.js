bc0f0b6b66bf565e062550634e9b0417cd71b2dd
Pete VS <pete_vs88@me.com>
2020-10-15 21:51:21 -0400

    got posts showing and search bar added

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@
 */
 
 // Import the state hook
-import React from 'react';
+import React, { useState } from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
+import Posts from './components/Posts/Posts.js'
+import SearchBar from './components/SearchBar/SearchBar.js'
 // Import the dummyData
+import dummyData from './dummy-data.js'
 import './App.css';
 
 const App = () => {
+  const [posts, setPosts] = useState(dummyData)
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
@@ -27,11 +31,14 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
+    // setPosts(posts.map(x => (posts.id === postId) ? ))
   };
 
   return (
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
+      <SearchBar />
+      <Posts posts={posts} />
       {/* Check the implementation of each component, to see what props they require, if any! */}
     </div>
   );
